860565d5b33f876d72571a266afb458317546f1f
moozzyk <pawelka@microsoft.com>
2017-04-04 14:37:56 -0700

    PR feedback

--- test/Microsoft.AspNetCore.SignalR.Client.Tests/TestTransportFactory.cs
+++ test/Microsoft.AspNetCore.SignalR.Client.Tests/TestTransportFactory.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.Logging;
 using System.Net.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.Sockets.Client.Tests
 {
